2c4f3fdac83e9406c9181bafc543c66e5b1032a2
dr3248 <116612055+dr3248@users.noreply.github.com>
2023-05-09 21:14:20 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,30 +1,30 @@
 const express = require("express");
-const https = require("https");
-const bodyParser = require("body-parser");
 const fs = require("fs");
 
 const app = express();
-app.use(bodyParser.urlencoded({extended: true}));
 
+app.get("/", (req, res)=> {
+    res.sendFile(__dirname + "/index.html");
+});
+
+//http://localhost:3000/trips
+//http://localhost:3000/trips?id=0
+//http://localhost:3000/trips?id=1
+//http://localhost:3000/trips?id=2
 app.get("/trips", (req, res) => {
     var data = fs.readFileSync('input.json', 'utf-8', (err) => {
         if (err) throw err;
-        // obj = JSON.parse(data);
-        // console.log(data);
     });
-    console.log(data);
+    // console.log(data);
     const respObj = JSON.parse(data);
-    // console.log('id: ' + req.query.q);
-    //http://localhost:3000/trips?q=Africa&p=5days&b=$500
     const id = req.query.id;
-    
     if(id === undefined) {
         res.send(respObj);
+    } else if (respObj[id] == undefined) {
+        res.status(404).send("Trip not found!")
     } else {
-        console.log("id = ", id);
         res.send(respObj[id]);
     }
-
 });
 
 app.listen(3000, (req, res) => {
