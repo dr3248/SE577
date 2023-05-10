9b46a0105e960bf33783c29e60ae90fdf2677124
dr3248 <116612055+dr3248@users.noreply.github.com>
2023-05-09 21:28:54 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -11,20 +11,33 @@ app.get("/", (req, res)=> {
 //http://localhost:3000/trips?id=0
 //http://localhost:3000/trips?id=1
 //http://localhost:3000/trips?id=2
+//http://localhost:3000/trips?dest=Africa
 app.get("/trips", (req, res) => {
     var data = fs.readFileSync('input.json', 'utf-8', (err) => {
         if (err) throw err;
     });
     // console.log(data);
     const respObj = JSON.parse(data);
-    const id = req.query.id;
-    if(id === undefined) {
-        res.send(respObj);
-    } else if (respObj[id] == undefined) {
-        res.status(404).send("Trip not found!")
+    const {id, dest} = req.query;
+    if(id) {
+        if(id === undefined) {
+            res.send(respObj);
+        } else if (respObj[id] == undefined) {
+            res.status(404).send("Trip not found!")
+        } else {
+            res.send(respObj[id]);
+        }
+    } else if (dest) {
+        const trip = respObj.find(element => element.destination == dest);
+        if (trip == undefined) {
+            res.status(404).send("Trip not found!")
+        } else {
+            res.send(trip);
+        }
     } else {
-        res.send(respObj[id]);
+        res.send(respObj);
     }
+
 });
 
 app.listen(3000, (req, res) => {
